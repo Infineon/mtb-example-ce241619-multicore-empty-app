e96414e4a9d7924bb47bff1e85780effa6cf7497
gitlab-runner <do-not-reply@cypress.com>
2026-09-20 16:53:18 -0500

    Upload mtb-example-ce241619-multicore-empty-app 1.0.0.565 [5423]

---
+++ ppca_cm33_0/main.c
@@ -0,0 +1,81 @@
+/******************************************************************************
+* File Name:   main.c
+*
+* Description: PPCA Core 0 starter stub for the Multicore Empty Application.
+*              Add Core 0 application logic inside the for(;;) loop.
+*              Use PPCA_M1_VAR_ADDRESS to exchange data with the main CM33 core.
+*
+*
+********************************************************************************
+* (c) 2026, Infineon Technologies AG, or an affiliate of Infineon
+* Technologies AG. All rights reserved.
+* This software, associated documentation and materials ("Software") is
+* owned by Infineon Technologies AG or one of its affiliates ("Infineon")
+* and is protected by and subject to worldwide patent protection, worldwide
+* copyright laws, and international treaty provisions. Therefore, you may use
+* this Software only as provided in the license agreement accompanying the
+* software package from which you obtained this Software. If no license
+* agreement applies, then any use, reproduction, modification, translation, or
+* compilation of this Software is prohibited without the express written
+* permission of Infineon.
+*
+* Disclaimer: UNLESS OTHERWISE EXPRESSLY AGREED WITH INFINEON, THIS SOFTWARE
+* IS PROVIDED AS-IS, WITH NO WARRANTY OF ANY KIND, EXPRESS OR IMPLIED,
+* INCLUDING, BUT NOT LIMITED TO, ALL WARRANTIES OF NON-INFRINGEMENT OF
+* THIRD-PARTY RIGHTS AND IMPLIED WARRANTIES SUCH AS WARRANTIES OF FITNESS FOR A
+* SPECIFIC USE/PURPOSE OR MERCHANTABILITY.
+* Infineon reserves the right to make changes to the Software without notice.
+* You are responsible for properly designing, programming, and testing the
+* functionality and safety of your intended application of the Software, as
+* well as complying with any legal requirements related to its use. Infineon
+* does not guarantee that the Software will be free from intrusion, data theft
+* or loss, or other breaches ("Security Breaches"), and Infineon shall have
+* no liability arising out of any Security Breaches. Unless otherwise
+* explicitly approved by Infineon, the Software may not be used in any
+* application where a failure of the Product or any consequences of the use
+* thereof can reasonably be expected to result in personal injury.
+*******************************************************************************/
+
+#include "cy_pdl.h"
+#include "cybsp.h"
+
+/*******************************************************************************
+* Macros
+********************************************************************************/
+
+
+/*******************************************************************************
+* Global Variables
+********************************************************************************/
+
+
+/*******************************************************************************
+* Function Prototypes
+********************************************************************************/
+
+
+/*******************************************************************************
+* Function Name: main
+********************************************************************************
+* Summary:
+* This is the main function for PPCA Core 0. Starter stub – the core enters
+* an idle loop immediately. Add Core 0 application logic inside the for(;;)
+* body. Use PPCA_M1_VAR_ADDRESS to exchange data with the main CM33 core.
+*
+* Parameters:
+*  void
+*
+* Return:
+*  int
+*
+*******************************************************************************/
+int main(void)
+{
+    /* PPCA Core 0 starter stub – add application logic here.
+       This core runs independently from the CM33 secure core.
+       Shared memory at PPCA_M1_VAR_ADDRESS can be used to exchange
+       data with the main CM33 core. */
+     for(;;)
+     {
+     }
+}
